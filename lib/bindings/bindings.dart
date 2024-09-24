235247bce9fc6602bc7b4767ae86372380802ad4
Aji Suryawan <ajisuryawan@smkrus.sch.id>
2024-09-24 09:31:37 +0700

    fix : [AS] add bindings example

--- lib/bindings/bindings.dart
+++ lib/bindings/bindings.dart
@@ -1,9 +1,13 @@
 import 'package:get/get.dart';
 import '../controllers/dashboard_controller.dart';
+import '../controllers/example_controller.dart';
 
 class MyBindings extends Bindings {
   @override
   void dependencies() {
     Get.lazyPut(() => DashboardController());
+    // another bindings
+    Get.lazyPut(() => ExampleController());
+    
   }
 }
--- lib/pages/menu/home_menu.dart
+++ lib/pages/menu/home_menu.dart
@@ -1,10 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:my_bottom_menu_example/controllers/example_controller.dart';
 
 class HomeMenu extends StatelessWidget {
   const HomeMenu({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final ExampleController exampleController = Get.find();
+
     return Scaffold(
       body: Center(child: Text("ini home menu")),
     );
